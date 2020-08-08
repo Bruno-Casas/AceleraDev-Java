63b446c51039f1ff58e0b8c1ed92a32659abdc20
Bruno de Almeida  Casas <brunocasas04@gmail.com>
2020-08-08 13:31:19 -0400

    Módulo 6. Desafio: Criando endpoints com Spring

---
+++ Sexto módulo/java-11/src/main/java/com/challenge/endpoints/advice/ControllerExceptionsHandler.java
@@ -0,0 +1,20 @@
+package com.challenge.endpoints.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ControllerAdvice
+public class ControllerExceptionsHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ResponseBody
+	public String handleResourceNotFoundException(ResourceNotFoundException ex) {
+		return ex.getMessage();
+	}
+
+}
---
+++ Sexto módulo/java-11/src/main/java/com/challenge/endpoints/advice/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.challenge.endpoints.advice;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -5625574484783064764L;
+
+	public ResourceNotFoundException(String resourceName) {
+		super("Resource: " + resourceName + " not found.");
+	}
+	
+}
